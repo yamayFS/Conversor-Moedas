275ce761c2d8c68d02a79d2748c1b0174622268b
Yamay Freitas <yamayfreitas@gmail.com>
2021-08-16 16:43:04 -0300

    Inclusão de API

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
-void main(){
+const request = "https://api.hgbrasil.com/finance/quotations?key=8d334f76";
+
+void main() async{
+
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)["results"]["currencies"]);
+
   runApp(MaterialApp(
     home: Container(),
   ));
