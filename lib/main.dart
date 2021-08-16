17dcfc81b0eef24a244711b6d33dbfb827af2923
Yamay Freitas <yamayfreitas@gmail.com>
2021-08-16 17:23:43 -0300

    Inclusão Future Builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance/quotations?key=8d334f76";
 
-void main() async{
+void main() async {
+  runApp(MaterialApp(home: Home()));
+}
 
+Future<Map> getData() async {
   http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]);
+  return json.decode(response.body);
+}
 
-  runApp(MaterialApp(
-    home: Container(),
-  ));
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("\$ Conversor \$"),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+        ),
+        body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch(snapshot.connectionState){
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text("Carregando Dados...",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 25.0),
+                    textAlign: TextAlign.center,)
+                );
+              default:
+                if(snapshot.hasError){
+                  return Center(
+                      child: Text("Erro ao carregar dados...",
+                        style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 25.0),
+                        textAlign: TextAlign.center,)
+                  );
+                } else{
+                  return Container(color: Colors.amber,);
+                }
+            }
+          })
+    );
+  }
 }
